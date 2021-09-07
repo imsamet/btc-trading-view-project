8396ac1db1f339ece33be44ecb47ec760d8484c5
imsamet <imsametcetin@gmail.com>
2021-09-07 08:47:29 +0300

    order-book design finish

--- src/components/order-book/order-book.js
+++ src/components/order-book/order-book.js
@@ -1,7 +1,83 @@
+import Style from './order-book.module.css'
+
+import Item from './item/item'
+
 function OrderBook() {
     return(
-        <div>
-            
+        <div className={Style.container}>
+            <div className={Style.content}>
+
+                <table className={Style.table}>
+
+                    <tr>
+                        <th>Fiyat (USDT)</th>
+                        <th>Miktar (BTC)</th>
+                        <th>Toplam</th>
+                    </tr>
+
+                    <Item color={"red"}/>
+                    <Item color={"red"}/>
+                    <Item color={"red"}/>
+                    <Item color={"red"}/>
+                    <Item color={"red"}/>
+                    <Item color={"red"}/>
+                    <Item color={"red"}/>
+                    <Item color={"red"}/>
+                    <Item color={"red"}/>
+                    <Item color={"red"}/>
+                    <Item color={"red"}/>
+                    <Item color={"red"}/>
+                    <Item color={"red"}/>
+                    <Item color={"red"}/>
+                    <Item color={"red"}/>
+                    <Item color={"red"}/>
+                    <Item color={"red"}/>
+                    <Item color={"red"}/>
+                    <Item color={"red"}/>
+                    <Item color={"red"}/>
+                    <Item color={"red"}/>
+ 
+                </table>
+
+            </div>
+
+            <div className={Style.content}>
+
+                <table className={Style.table}>
+
+                    <tr>
+                        <th>Fiyat (USDT)</th>
+                        <th>Miktar (BTC)</th>
+                        <th>Toplam</th>
+                    </tr>
+
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+                    <Item color={"green"}/>
+
+                </table>
+
+            </div>
         </div>
     )
 }
